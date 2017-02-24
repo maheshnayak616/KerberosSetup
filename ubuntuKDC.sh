ea392be03e9f36ab85a2e3cc0cf8ff9f575b9ae1
maheshnayak616 <mahesh.nayak@flipkart.com>
2017-02-24 11:15:28 +0530

    Updating

--- ubuntuKDC.sh
+++ ubuntuKDC.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # https://community.hortonworks.com/storage/attachments/7636-install-kdc-ubuntush.txt
 # Install packages
+#!/bin/sh
+
+# Install packages
 echo "Installing Kerberos Packages"
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
@@ -14,12 +17,10 @@ service krb5-admin-server start
 # #################################
 
 # Create krb5.conf file
-export host=$(hostname -f)
-export realm=${realm:-EXAMPLE.COM}
-export domain=${domain:-example.com}
-export kdcpassword=${kdcpassword:-BadPass#1}
-
-echo "Creating krb5.conf file, assuming KDC host is ${host} and realm is ${realm}"
+HOSTNAME=`hostname`
+REALM="EXAMPLE.COM"
+PASSWORD="BadPass#1"
+echo "Creating krb5.conf file, assuming KDC host is ${HOSTNAME} and realm is ${REALM}" 
 cat >/etc/krb5.conf <<EOF
 [logging]
  default = FILE:/var/log/krb5libs.log
@@ -27,7 +28,7 @@ cat >/etc/krb5.conf <<EOF
  admin_server = FILE:/var/log/kadmind.log
 
 [libdefaults]
- default_realm = ${realm}
+ default_realm = ${REALM}
  dns_lookup_realm = false
  dns_lookup_kdc = false
  ticket_lifetime = 24h
@@ -35,37 +36,33 @@ cat >/etc/krb5.conf <<EOF
  forwardable = true
 
 [realms]
- ${realm} = {
-  kdc = ${host}
-  admin_server = ${host}
+ ${REALM} = {
+  kdc = ${HOSTNAME}
+  admin_server = ${HOSTNAME}
  }
 
 [domain_realm]
- .${host} = ${realm}
- ${host} = ${realm}
+ .${HOSTNAME} = ${REALM}
+ ${HOSTNAME} = ${REALM}
 EOF
 
-echo "Creating kdc.conf file, assuming realm is ${realm}"
+echo "Creating kdc.conf file, assuming realm is ${REALM}"
 cat >/etc/krb5kdc/kadm5.acl <<EOF
-*/admin@${realm}	*
+*/admin@${REALM}	*
 EOF
-
-echo $kdcpassword > passwd
-echo $kdcpassword >> passwd
 
 # Create KDC database
 echo "Created KDC database, this could take some time"
 echo "HRNGDEVICE=/dev/uransom" > /etc/default/rng-tools
 /etc/init.d/rng-tools start
 mkdir -p /etc/krb5kdc
-kdb5_util create -s < passwd
+kdb5_util create -s -P ${PASSWORD}
 
 # Create admistrative user
 echo "Creating administriative account:"
 echo "  principal:  admin/admin"
-echo "  password:   $kdcpassword"
-kadmin.local -q "addprinc admin/admin" < passwd
-rm -f passwd
+echo "  password:   ${PASSWORD}"
+kadmin.local -q 'addprinc -pw ${PASSWORD} admin/admin'
 
 update-rc.d krb5-kdc defaults
 update-rc.d krb5-admin-server defaults
